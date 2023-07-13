8512f0f780d8cf6c14f447175c33ff0efba3614e
ykztenoks <danielskonetzky7@gmail.com>
2023-07-13 18:54:26 -0300

    :sparkles: we got validation!!

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const mongoose = require("mongoose");
 const User = require("../models/user.model");
 const bcrypt = require("bcryptjs");
 const router = new Router();
@@ -11,6 +12,25 @@ router.get("/signup", (req, res) => {
 router.post("/signup", (req, res, next) => {
   const { username, email, password } = req.body;
 
+  if (!username || !email || !password) {
+    res.render("auth/signup", {
+      errorMessage:
+        "All fields are mandatory. Please provide your username, email and password.",
+    });
+    return;
+  }
+
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res
+      .status(500)
+      .render("auth/signup", {
+        errorMessage:
+          "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
+      });
+    return;
+  }
+
   bcrypt
     .genSalt(salt)
     .then((salts) => {
@@ -21,7 +41,21 @@ router.post("/signup", (req, res, next) => {
         (createdUser) => res.redirect("/auth/userProfile")
       )
     )
-    .catch((err) => console.log(err));
+    .catch((error) => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render("auth/signup", { errorMessage: error.message });
+      } else if (error.code === 11000) {
+        console.log(
+          " Username and email need to be unique. Either username or email is already used. "
+        );
+
+        res.status(500).render("auth/signup", {
+          errorMessage: "User already exists.",
+        });
+      } else {
+        next(error);
+      }
+    });
 });
 
 router.get("/userProfile", (req, res) => {
